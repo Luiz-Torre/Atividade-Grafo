7792ca971529650e136bc47db0d67bc1c7760465
Luiz Torre <luiztorre@id.uff.br>
2021-12-06 20:52:38 -0300

    Adicionando aresta e imprimindo

--- grafo.c
+++ grafo.c
@@ -14,18 +14,55 @@ void inicializar(lista **g, int n){
         g[i] = NULL;
     }
 }
+
+lista *InserirLista(lista *l, int d, int c){
+    lista *no = (lista *)malloc(sizeof(lista));
+    no -> destino = d; no -> custo = c;
+    no -> prox = 1;
+    return no;
+}
+
+void InserirAresta(lista **g, int origem, int destino, int custo){
+    g[origem] = InserirLista(g[origem], destino, custo);
+}
+
+
+
+void ImprimirLista(lista *l){
+    if(l!= NULL){
+        printf("-(%d, %d)", l-> destino, l->custo);
+        ImprimirLista(l->prox);
+    }
+}
+
+
+void ImprimirGrafo(lista **g, int n){
+    int i;
+    printf("Grafo")
+    for(i = 1;i<n;i++){
+        printf("\n\t%d", i);
+        ImprimirLista(g[i]);
+    }
+}
+
 int main(){
-    int var;
-    
+    int var, tam;
+    int origem, destino, custo;
+    lista **g;
+    puts("Informe quantos nós terá seu grafo: \n");
+    scanf("%d", &tam);
+    inicializar(g, tam);
     printf("Informe: \n 1- Inserir Arestra\n 2- Remover uma aresta\n 3- Imprimir grafo\n 4- Imprimir os graus de entrada e saída de um vértice\n 5- Verificar se um grafo é completo\n 6- Imprimir todos os caminhos entre uma origem e um destino\n 7- Imprimir o caminho mais curto\n 8- Imprimir o caminho de menor custo\n 9- Sair");
     scanf("%d", &var);
     while(var != 9){
         switch(var){
             case 1:
-                break;
+                InserirAresta(g, origem, destino, custo);                break;
             case 2:
+
                 break;
             case 3:
+                ImprimirGrafo(g, tam);
                 break;
             case 4:
                 break;
