7cd568dad7e3041e8e2f4e51e4f9b1589320dd0e
Luiz Torre <luiztorre@id.uff.br>
2021-12-07 15:47:11 -0300

    Adicionando o n para formatação

--- grafo.c
+++ grafo.c
@@ -136,7 +136,7 @@ int main(){
                 printf("\nValor Invalido");
                 break;
         }
-        printf("Informe: \n 1- Inserir Aresta\n 2- Remover uma aresta\n 3- Imprimir grafo\n 4- Imprimir os graus de entrada e saída de um vértice\n 5- Verificar se um grafo é completo\n 6- Imprimir todos os caminhos entre uma origem e um destino\n 7- Imprimir o caminho mais curto\n 8- Imprimir o caminho de menor custo\n 9- Sair");
+        printf("\nInforme: \n 1- Inserir Aresta\n 2- Remover uma aresta\n 3- Imprimir grafo\n 4- Imprimir os graus de entrada e saída de um vértice\n 5- Verificar se um grafo é completo\n 6- Imprimir todos os caminhos entre uma origem e um destino\n 7- Imprimir o caminho mais curto\n 8- Imprimir o caminho de menor custo\n 9- Sair\n");
         scanf("%d", &var);
     }
     return 0;
