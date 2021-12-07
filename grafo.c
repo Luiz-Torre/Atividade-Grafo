d5f03bcdff4e457939bcb98065c37ffef1ea660f
HunterXVD11 <81260156+HunterXVD11@users.noreply.github.com>
2021-12-07 15:17:11 -0300

    Tentando fazer

--- grafo.c
+++ grafo.c
@@ -16,7 +16,7 @@ void inicializar(lista **g, int n){
 lista *InserirLista(lista *l, int d, int c){
     lista *no = (lista *)malloc(sizeof(lista));
     no -> destino = d; no -> custo = c;
-    no -> prox = 1;
+    no -> prox = l;
     return no;
 }
 
@@ -34,21 +34,55 @@ void ImprimirLista(lista *l){
 void ImprimirGrafo(lista **g, int n){
     int i;
     printf("Grafo");
-    for(i = 1;i<n;i++){
+    for(i = 1;i<n+1;i++){
         printf("\n\t%d", i);
         ImprimirLista(g[i]);
     }
 }
+void MostraGraus(lista *g,int tam,int *vetE,int *vetS){
+    for(int i = 1;i<tam+1;i++){
 
+
+    }
+//    for(int i = 0; i< tam;i++){
+//        printf("%d",vetE[i]);
+//    }
+//    printf("\n");
+    for (int i = 0; i < tam; i++) {
+        for (int j = 0; j < tam; j++) {
+            printf("%d",g[i+1]->destino);
+            printf("\n");
+            if (g[i+1]->destino == j+1){
+                vetS[i]+= 1;
+                vetE[j]+= 1;
+            }
+        }
+    }
+//    for(int i = 0; i< tam;i++){
+//        printf("%d",vetE[i]);
+//    }
+//    printf("\n");
+    for (int i = 0; i < tam; ++i) {
+        printf("Vertice %d\n", (i+1));
+        printf("gs (Grau de saida): %d\n",vetS[i]);
+        printf("ge (Grau de entrada): %d\n",vetE[i]);
+    }
+}
 int main(){
     int var, tam;
     int origem, destino, custo;
     lista **g;
     puts("Informe quantos nós terá seu grafo: \n");
     scanf("%d", &tam);
+    int vetE[tam];
+    int vetS[tam];
+    for(int i = 0; i< tam;i++){
+        vetE[i] = 0;
+        vetS[i] = 0;
+    }
     g = (lista**)malloc((tam+1)*sizeof(lista*));
     inicializar(g, tam);
-    printf("Informe: \n 1- Inserir Arestra\n 2- Remover uma aresta\n 3- Imprimir grafo\n 4- Imprimir os graus de entrada e saída de um vértice\n 5- Verificar se um grafo é completo\n 6- Imprimir todos os caminhos entre uma origem e um destino\n 7- Imprimir o caminho mais curto\n 8- Imprimir o caminho de menor custo\n 9- Sair");
+    printf("Informe: \n 1- Inserir Aresta\n 2- Remover uma aresta\n 3- Imprimir grafo\n 4- Imprimir os graus de entrada e saída de um vértice\n 5- Verificar se um grafo é completo\n 6- Imprimir todos os caminhos entre uma origem e um destino\n 7- Imprimir o caminho mais curto\n 8- Imprimir o caminho de menor custo\n 9- Sair");
     scanf("%d", &var);
     while(var != 9){
         switch(var){
@@ -66,6 +100,7 @@ int main(){
                 ImprimirGrafo(g, tam);
                 break;
             case 4:
+                MostraGraus(g,tam,vetE,vetS);
                 break;
             case 5:
                 break;
@@ -81,8 +116,15 @@ int main(){
                 printf("\nValor Invalido \n");
                 break;
         }
-        printf("Informe: \n 1- Inserir Arestra\n 2- Remover uma aresta\n 3- Imprimir grafo\n 4- Imprimir os graus de entrada e saída de um vértice\n 5- Verificar se um grafo é completo\n 6- Imprimir todos os caminhos entre uma origem e um destino\n 7- Imprimir o caminho mais curto\n 8- Imprimir o caminho de menor custo\n 9- Sair");
+        printf("Informe: \n 1- Inserir Aresta\n 2- Remover uma aresta\n 3- Imprimir grafo\n 4- Imprimir os graus de entrada e saída de um vértice\n 5- Verificar se um grafo é completo\n 6- Imprimir todos os caminhos entre uma origem e um destino\n 7- Imprimir o caminho mais curto\n 8- Imprimir o caminho de menor custo\n 9- Sair");
         scanf("%d", &var);
     }
     return 0;
 }
+
+//1 1 2 4
+//1 2 1 7
+//1 1 3 9
+//1 3 1 10
+//1 2 3 11
+//1 3 2 3
