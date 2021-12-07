bacf360e5f62158c3fb4ec3c01e381d8a960be74
Luiz Torre <luiztorre@id.uff.br>
2021-12-07 18:56:41 -0300

    Adicionando o free

--- grafo.c
+++ grafo.c
@@ -119,6 +119,12 @@ void TestaCompleta(lista **g,int tam){
         puts("O grafo é completo");
 }
 
+void limpa(lista **g, int tam){
+  for(int i=0; i<=tam;i++) {
+    free(g[i]);
+  }
+  free(g);
+}
 int main(){
     int var, tam;
     int origem, destino, custo, aux, aux_dest;
@@ -160,6 +166,7 @@ int main(){
                 TestaCompleta(g,tam);
                 break;
             case 6:
+                limpa(g, tam);
                 exit(0);
                 break;
             default:
@@ -171,5 +178,3 @@ int main(){
     }
     return 0;
 }
-
-//3 1 1 2 4 1 2 1 7 1 1 3 9 1 3 1 10 1 2 3 11 1 3 2 3
